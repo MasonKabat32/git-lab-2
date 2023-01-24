7a53185d87ba96457c6caf2877b35b8d923defd7
MasonKabat <mk964421@ohio.edu>
2023-01-24 15:53:07 -0500

    Program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mason Kabat
+ *        Date: 1/24/23
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+  cout << "Hello Git!!" << endl;
+  return 0;
+}
